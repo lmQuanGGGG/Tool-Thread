462237535a62d5e22afc775115b66ff920687c75
Lê Minh Quang <wang04@ES-QuangLM23.local>
2026-07-11 17:42:45 +0700

    Add static User-Agent to prevent Facebook cookie invalidation

--- Tool-Thread/tele_bot/fb_bot/4_fb_shopee_post_story.js
+++ Tool-Thread/tele_bot/fb_bot/4_fb_shopee_post_story.js
@@ -164,6 +164,7 @@ const { fetchBotConfig, logToWeb, checkQuota, updateUsageStats, sendTelegramMess
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-notifications']
     });
     const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
     await page.setViewport({ width: 1280, height: 800 });
 
     // 2. Đăng nhập Facebook
